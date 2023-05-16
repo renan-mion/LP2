8120072102ac55a6345014dffb8db873d00e4e8b
renan-mion <89620634+renan-mion@users.noreply.github.com>
2023-05-16 12:00:08 -0300

    Add files via upload

---
+++ Prova/PNomes/PNomes/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PNomes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstbxNomes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(211, 251);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(239, 110);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.BtnExecutar_Click);
+            // 
+            // lstbxNomes
+            // 
+            this.lstbxNomes.FormattingEnabled = true;
+            this.lstbxNomes.ItemHeight = 20;
+            this.lstbxNomes.Location = new System.Drawing.Point(576, 158);
+            this.lstbxNomes.Name = "lstbxNomes";
+            this.lstbxNomes.Size = new System.Drawing.Size(446, 344);
+            this.lstbxNomes.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 641);
+            this.Controls.Add(this.lstbxNomes);
+            this.Controls.Add(this.btnExecutar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstbxNomes;
+    }
+}
+
---
+++ Prova/PNomes/PNomes/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PNomes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExecutar_Click(object sender, EventArgs e)
+        {
+
+            string[] nomes = new string[3];
+            int[] comprimento = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                nomes[i] = Interaction.InputBox("Digite o nome completo:");
+                if (nomes[i] == string.Empty || nomes[i].Length < 2)
+                {
+                    MessageBox.Show("Digite um nome válido");
+                    i--;
+                }
+                else
+                {
+                    comprimento[i] = nomes[i].Replace(" ", "").Length;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                lstbxNomes.Items.Add("O nome: " + nomes[i] + " tem " + comprimento[i] + " caracteres");
+            }
+
+        }
+    }
+}
